f7cb6d17da3520eb2a2a85b1af0d47fbe1d817f1
SamuraiViking <nelson67@stolaf.edu>
2019-11-06 22:18:09 -0600

    add CafFirstFloor component

---
+++ src/CafFirstFloor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CafFirstFloor extends Component {
+    render() {
+        return (
+            <h1>CafFirstFloor</h1>
+        )
+    }
+}
+
+export default CafFirstFloor;
